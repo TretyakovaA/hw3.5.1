fc83935daa0e4cf06451d5eafd54b371616ff5a4
TretyakovaA <putinvivo@bk.ru>
2022-12-18 15:35:08 +0300

    Initial commit

---
+++ hw3.5.1/src/test/java/com/example/hw351/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw351;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
